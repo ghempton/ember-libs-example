c676ea3edb7f843b099d9967f96e28be0502510c
Gordon L. Hempton <ghempton@gmail.com>
2014-03-05 22:38:33 -0800

    customizable input types

--- vendor/input-for.js
+++ vendor/input-for.js
@@ -1,10 +1,14 @@
-var InputFor = Ember.Component.extend({
+var Input = Ember.Component.extend({
   classNameBindings: [':input-container', 'error:error'],
-  layoutName: 'input-for',
+  layoutName: 'inputs/input',
   label: Ember.computed(function() {
     var name = this.get('name');
     return name.charAt(0).toUpperCase() + name.slice(1) + ":";
   }).property('name')
+});
+
+var Text = Input.extend({
+  layoutName: 'inputs/text',
 });
 
 function inputForHelper(path, options) {
@@ -12,12 +16,18 @@ function inputForHelper(path, options) {
   options.hash.valueBinding = path;
   options.hash.errorBinding = "errors." + path;
 
-  return Ember.Handlebars.helpers.view.call(this, InputFor, options);
+  var typeKey = options.hash.type || 'input',
+      container = options.data.view.container,
+      View = container.lookupFactory('input:' + typeKey);
+
+  return Ember.Handlebars.helpers.view.call(this, View, options);
 }
 
 Ember.Application.initializer({
   name: 'inputs',
   initialize: function(container, application) {
     container.register('helper:input-for', inputForHelper);
+    container.register('input:input', Input);
+    container.register('input:text', Text);
   }
 });
