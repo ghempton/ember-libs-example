56fe9f0bf5f3e0f6a0423430e2d6695d42c6d9a1
Gordon L. Hempton <ghempton@gmail.com>
2014-03-05 22:24:42 -0800

    switch input-for to helper

--- vendor/input-for.js
+++ vendor/input-for.js
@@ -1,11 +1,23 @@
 var InputFor = Ember.Component.extend({
   classNameBindings: [':input-container', 'error:error'],
-  layoutName: 'input-for'
+  layoutName: 'input-for',
+  label: Ember.computed(function() {
+    var name = this.get('name');
+    return name.charAt(0).toUpperCase() + name.slice(1) + ":";
+  }).property('name')
 });
+
+function inputForHelper(path, options) {
+  options.hash.name = path;
+  options.hash.valueBinding = path;
+  options.hash.errorBinding = "errors." + path;
+
+  return Ember.Handlebars.helpers.view.call(this, InputFor, options);
+}
 
 Ember.Application.initializer({
   name: 'inputs',
   initialize: function(container, application) {
-    container.register('component:input-for', InputFor);
+    container.register('helper:input-for', inputForHelper);
   }
 });
